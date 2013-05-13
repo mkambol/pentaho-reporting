57c4992c3db28bd5defa9c237fa36b9d82cc9074
Thomas Morgner <git@rabbit-stew-dio.com>
2013-05-13 16:05:38 +0100

    PRD-4216: Test case updated a bit for relational reports

--- engine/core/test/org/pentaho/reporting/engine/classic/core/function/TotalPageItemCountFunctionTest.java
+++ engine/core/test/org/pentaho/reporting/engine/classic/core/function/TotalPageItemCountFunctionTest.java
@@ -22,10 +22,13 @@ import javax.swing.table.TableModel;
 import junit.framework.TestCase;
 import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
 import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.SimplePageDefinition;
 import org.pentaho.reporting.engine.classic.core.TableDataFactory;
 import org.pentaho.reporting.engine.classic.core.designtime.DesignTimeDataSchemaModel;
 import org.pentaho.reporting.engine.classic.core.elementfactory.CrosstabBuilder;
+import org.pentaho.reporting.engine.classic.core.testsupport.RelationalReportBuilder;
 import org.pentaho.reporting.engine.classic.core.testsupport.DebugReportRunner;
+import org.pentaho.reporting.engine.classic.core.util.PageSize;
 import org.pentaho.reporting.engine.classic.core.util.TypedTableModel;
 
 /** @noinspection HardCodedStringLiteral*/
@@ -37,6 +40,8 @@ public class TotalPageItemCountFunctionTest extends TestCase
   public static final String COLUMN_DIMENSION_A = "Column-Dimension-A";
   public static final String COLUMN_DIMENSION_B = "Column-Dimension-B";
   public static final String VALUE = "Value";
+
+  private static final boolean failHard = false;
 
   public TotalPageItemCountFunctionTest()
   {
@@ -52,7 +57,7 @@ public class TotalPageItemCountFunctionTest extends TestCase
     ClassicEngineBoot.getInstance().start();
   }
 
-  private TableModel createTableModel()
+  private TableModel createCrosstabTableModel()
   {
     final TypedTableModel model = new TypedTableModel();
     model.addColumn(ROW_DIMENSION_A, String.class);
@@ -60,12 +65,12 @@ public class TotalPageItemCountFunctionTest extends TestCase
     model.addColumn(COLUMN_DIMENSION_A, String.class);
     model.addColumn(COLUMN_DIMENSION_B, String.class);
     model.addColumn(VALUE, String.class);
-    model.addColumn("validate-cell-sum", String.class);
-    model.addColumn("validate-detail-sum", String.class);
-    model.addColumn("validate-row-b-sum", String.class);
-    model.addColumn("validate-row-a-sum", String.class);
-    model.addColumn("validate-column-a-sum", String.class); // should always be the same as row-b-sum
-    model.addColumn("validate-column-b-sum", String.class);
+    model.addColumn("validate-cell-sum", Integer.class);
+    model.addColumn("validate-detail-sum", Integer.class);
+    model.addColumn("validate-row-b-sum", Integer.class);
+    model.addColumn("validate-row-a-sum", Integer.class);
+    model.addColumn("validate-column-a-sum", Integer.class); // should always be the same as row-b-sum
+    model.addColumn("validate-column-b-sum", Integer.class);
 
     model.addRow("R1", "r1", "C1", "c1", 1, 1, 1, 2, 4, 2, 2);
     model.addRow("R1", "r1", "C1", "c2", 2, 1, 1, 2, 4, 2, 2);
@@ -88,10 +93,41 @@ public class TotalPageItemCountFunctionTest extends TestCase
     return model;
   }
 
+  private TableModel createRelationalTableModel()
+  {
+    final TypedTableModel model = new TypedTableModel();
+    model.addColumn(ROW_DIMENSION_A, String.class);
+    model.addColumn(ROW_DIMENSION_B, String.class);
+    model.addColumn(VALUE, String.class);
+    model.addColumn("validate-row-b-sum", Integer.class);
+    model.addColumn("validate-row-a-sum", Integer.class);
+
+    model.addRow("RA", "r1", 1, 11, 11);
+    model.addRow("RA", "r2", 2, 11, 11);
+    model.addRow("RA", "r1", 1, 11, 11);
+    model.addRow("RA", "r1", 1, 11, 11);
+    model.addRow("RA", "r1", 2, 11, 11);
+    model.addRow("RA", "r2", 1, 11, 11);
+    model.addRow("RA", "r2", 1, 11, 11);
+    model.addRow("RA", "r2", 2, 11, 11);
+    model.addRow("RB", "r1", 1, 11, 11);
+    model.addRow("RB", "r1", 1, 11, 11);
+    model.addRow("RB", "r1", 1, 11, 11);
+    model.addRow("RB", "r1", 2, 11, 11);
+    model.addRow("RB", "r1", 1, 11, 11);
+    model.addRow("RB", "r2", 2, 11, 11);
+    model.addRow("RB", "r2", 1, 12, 12);
+    model.addRow("RB", "r2", 1, 12, 12);
+    model.addRow("RB", "r2", 1, 12, 12);
+    model.addRow("RB", "r2", 2, 12, 12);
+    model.addRow("RB", "r2", 2, 12, 12);
+    model.addRow("RB", "r2", 2, 12, 12);
+    return model;
+  }
+
   private AggregationFunction create(final String name,
-                                          final String field,
-                                          final String filter,
-                                          final String group)
+                                     final String filter,
+                                     final String group)
   {
     final TotalPageItemCountFunction detailsSum = new TotalPageItemCountFunction();
     detailsSum.setName(name);
@@ -101,29 +137,27 @@ public class TotalPageItemCountFunctionTest extends TestCase
     return detailsSum;
   }
 
+  private AggregationFunction create(final String name,
+                                     final String group)
+  {
+    final TotalPageItemCountFunction detailsSum = new TotalPageItemCountFunction();
+    detailsSum.setName(name);
+    detailsSum.setGroup(group);
+    detailsSum.setDependencyLevel(1);
+    return detailsSum;
+  }
+
 
   public void testRelationalReport() throws Exception
   {
-    final TableModel tableModel = createTableModel();
-    final MasterReport report = createCrosstabReport(tableModel);
+    final TableModel tableModel = createRelationalTableModel();
+    final MasterReport report = createRelationalReport(tableModel);
 
-    report.addExpression(create("cell-sum", VALUE, null, COLUMN_DIMENSION_B));
-    report.addExpression(new ValidatePageFunctionResultExpression("#cell-sum", true, null));
+    report.addExpression(create("row-b-sum", ROW_DIMENSION_B));
+    report.addExpression(new ValidatePageFunctionResultExpression("#row-b-sum", failHard, null));
 
-    report.addExpression(create("detail-sum", VALUE, COLUMN_DIMENSION_B, ROW_DIMENSION_B));
-    report.addExpression(new ValidatePageFunctionResultExpression("#detail-sum", true, COLUMN_DIMENSION_B));
-
-    report.addExpression(create("row-b-sum", VALUE, COLUMN_DIMENSION_A, ROW_DIMENSION_B));
-    report.addExpression(new ValidatePageFunctionResultExpression("#row-b-sum", true, COLUMN_DIMENSION_A));
-
-    report.addExpression(create("column-a-sum", VALUE, null, COLUMN_DIMENSION_A));
-    report.addExpression(new ValidatePageFunctionResultExpression("#column-a-sum", true, null));
-
-    report.addExpression(create("row-a-sum", VALUE, COLUMN_DIMENSION_A, ROW_DIMENSION_A));
-    report.addExpression(new ValidatePageFunctionResultExpression("#row-a-sum", true, COLUMN_DIMENSION_A));
-
-    report.addExpression(create("column-b-sum", VALUE, COLUMN_DIMENSION_B, ROW_DIMENSION_A));
-    report.addExpression(new ValidatePageFunctionResultExpression("#column-b-sum", true, COLUMN_DIMENSION_B));
+    report.addExpression(create("row-a-sum", ROW_DIMENSION_A));
+    report.addExpression(new ValidatePageFunctionResultExpression("#row-a-sum", failHard, null));
 
     DebugReportRunner.showDialog(report);
 
@@ -132,26 +166,26 @@ public class TotalPageItemCountFunctionTest extends TestCase
 
   public void testCrosstabReport() throws Exception
   {
-    final TableModel tableModel = createTableModel();
+    final TableModel tableModel = createCrosstabTableModel();
     final MasterReport report = createCrosstabReport(tableModel);
 
-    report.addExpression(create("cell-sum", VALUE, null, COLUMN_DIMENSION_B));
-    report.addExpression(new ValidatePageFunctionResultExpression("#cell-sum", true, null));
+    report.addExpression(create("cell-sum", null, COLUMN_DIMENSION_B));
+    report.addExpression(new ValidatePageFunctionResultExpression("#cell-sum", failHard, null));
 
-    report.addExpression(create("detail-sum", VALUE, COLUMN_DIMENSION_B, ROW_DIMENSION_B));
-    report.addExpression(new ValidatePageFunctionResultExpression("#detail-sum", true, COLUMN_DIMENSION_B));
+    report.addExpression(create("detail-sum", COLUMN_DIMENSION_B, ROW_DIMENSION_B));
+    report.addExpression(new ValidatePageFunctionResultExpression("#detail-sum", failHard, COLUMN_DIMENSION_B));
 
-    report.addExpression(create("row-b-sum", VALUE, COLUMN_DIMENSION_A, ROW_DIMENSION_B));
-    report.addExpression(new ValidatePageFunctionResultExpression("#row-b-sum", true, COLUMN_DIMENSION_A));
+    report.addExpression(create("row-b-sum", COLUMN_DIMENSION_A, ROW_DIMENSION_B));
+    report.addExpression(new ValidatePageFunctionResultExpression("#row-b-sum", failHard, COLUMN_DIMENSION_A));
 
-    report.addExpression(create("column-a-sum", VALUE, null, COLUMN_DIMENSION_A));
-    report.addExpression(new ValidatePageFunctionResultExpression("#column-a-sum", true, null));
+    report.addExpression(create("column-a-sum", null, COLUMN_DIMENSION_A));
+    report.addExpression(new ValidatePageFunctionResultExpression("#column-a-sum", failHard, null));
 
-    report.addExpression(create("row-a-sum", VALUE, COLUMN_DIMENSION_A, ROW_DIMENSION_A));
-    report.addExpression(new ValidatePageFunctionResultExpression("#row-a-sum", true, COLUMN_DIMENSION_A));
+    report.addExpression(create("row-a-sum", COLUMN_DIMENSION_A, ROW_DIMENSION_A));
+    report.addExpression(new ValidatePageFunctionResultExpression("#row-a-sum", failHard, COLUMN_DIMENSION_A));
 
-    report.addExpression(create("column-b-sum", VALUE, COLUMN_DIMENSION_B, ROW_DIMENSION_A));
-    report.addExpression(new ValidatePageFunctionResultExpression("#column-b-sum", true, COLUMN_DIMENSION_B));
+    report.addExpression(create("column-b-sum", COLUMN_DIMENSION_B, ROW_DIMENSION_A));
+    report.addExpression(new ValidatePageFunctionResultExpression("#column-b-sum", failHard, COLUMN_DIMENSION_B));
 
     DebugReportRunner.showDialog(report);
 
@@ -161,12 +195,13 @@ public class TotalPageItemCountFunctionTest extends TestCase
   private MasterReport createCrosstabReport(final TableModel tableModel)
   {
     final MasterReport report = new MasterReport();
+    report.setPageDefinition(new SimplePageDefinition(new PageSize(800, 400)));
     report.setDataFactory(new TableDataFactory("query", tableModel));
     report.setQuery("query");
     final DesignTimeDataSchemaModel dataSchemaModel = new DesignTimeDataSchemaModel(report);
 
     final CrosstabBuilder builder = new CrosstabBuilder(dataSchemaModel);
-    builder.addRowDimension("Row-Dimension-A");
+    builder.addRowDimension(ROW_DIMENSION_A);
     builder.addRowDimension(ROW_DIMENSION_B);
     builder.addColumnDimension(COLUMN_DIMENSION_A);
     builder.addColumnDimension(COLUMN_DIMENSION_B);
@@ -178,16 +213,18 @@ public class TotalPageItemCountFunctionTest extends TestCase
   private MasterReport createRelationalReport(final TableModel tableModel)
   {
     final MasterReport report = new MasterReport();
+    report.setPageDefinition(new SimplePageDefinition(new PageSize(800, 300)));
     report.setDataFactory(new TableDataFactory("query", tableModel));
     report.setQuery("query");
-    final DesignTimeDataSchemaModel dataSchemaModel = new DesignTimeDataSchemaModel(report);
 
-    final CrosstabBuilder builder = new CrosstabBuilder(dataSchemaModel);
-    builder.addRowDimension("Row-Dimension-A");
-    builder.addRowDimension(ROW_DIMENSION_B);
-    builder.addColumnDimension(COLUMN_DIMENSION_A);
-    builder.addColumnDimension(COLUMN_DIMENSION_B);
-    builder.addDetails(VALUE, null);
+    final DesignTimeDataSchemaModel dataSchemaModel = new DesignTimeDataSchemaModel(report);
+    final RelationalReportBuilder builder = new RelationalReportBuilder(dataSchemaModel);
+    builder.addGroup(ROW_DIMENSION_A);
+    builder.addGroup(ROW_DIMENSION_B);
+    builder.addDetails(VALUE, TotalPageItemCountFunction.class);
+    builder.addDetails("row-a-sum", null);
+    builder.addDetails("row-b-sum", null);
+
     report.setRootGroup(builder.create());
     return report;
   }
